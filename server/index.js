9314b77abdee58b796603261e7a55662d4e9d56d
Billy Zhou <williamz.zhou1@gmail.com>
2019-07-18 12:57:54 -0700

    stitched three components together: checkout, reviews, and description

--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 var httpProxy = require('http-proxy');
 var apiProxy = httpProxy.createProxyServer();
-const port = 3000;
-const serverOne = 'http://172.17.0.4:3001',
-      serverTwo = 'http://172.17.0.3:3002';
+const port = 3000,
+      checkout = 'http://localhost:3001',
+      reviews = 'http://localhost:3002',
+      description = 'http://localhost:3003';
 
 express()
     .use(express.static('public'))
-    .all('/rooms(/dates)?', (req, res) => apiProxy.web(req, res, {target: serverOne}))
-    .all('/reviews', (req, res) => apiProxy.web(req, res, {target: serverTwo}))
-    .all('/search', (req, res) => apiProxy.web(req, res, {target: serverTwo}))
+    .all('/rooms(/dates)?', (req, res) => apiProxy.web(req, res, {target: checkout}))
+    .all('/reviews', (req, res) => apiProxy.web(req, res, {target: reviews}))
+    .all('/search', (req, res) => apiProxy.web(req, res, {target: reviews}))
+    .all('/getData', (req, res) => apiProxy.web(req, res, {target: description}))
     .listen(port, () => console.log(`listening at ${port}`))
